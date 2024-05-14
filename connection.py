977c12cccf3a2ebcc45833bca1dae59b64472642
DutchOzz <m.schut@student.utwente.nl>
2024-05-14 12:05:21 +0200

    added tablefunctions.py

---
+++ connection.py
@@ -0,0 +1,18 @@
+import psycopg2
+import config
+
+# Establish a connection to the database
+def connect():
+    conn = psycopg2.connect (
+        host = config.hostname, 
+        database = config.database, 
+        user = config.username, 
+        password = config.pwd, 
+        port = config.port_id
+        )
+    print("Connected to the database")
+    return conn
+
+# Close the connection to the database
+def close(conn):
+    conn.close()
---
+++ tableFunctions.py
@@ -0,0 +1,48 @@
+import psycopg2
+import connection
+
+# Create a table in the database
+def create_table(conn, schemaName, tableName):
+    cur = conn.cursor()
+
+    create_table_query = """
+        create table """ + schemaName + "." + tableName +  """ (like pp2324_37.drives including all);
+    """
+
+    cur.execute(create_table_query)
+    cur.close()
+
+# Drop a table from the database
+def drop_table(conn, schemaName, tableName):
+    cur = conn.cursor()
+
+    drop_table_query = """
+        drop table """ + schemaName + "." + tableName +  """;
+    """
+
+    cur.execute(drop_table_query)
+    cur.close()
+
+# Add a column to a table in the database
+def add_column(conn, schemaName, tableName, columnName, columnType):
+    cur = conn.cursor()
+
+    add_column_query = """
+        alter table """ + schemaName + "." + tableName +  """
+        add column """ + columnName + """ """ + columnType + """;
+    """
+
+    cur.execute(add_column_query)
+    cur.close()
+
+# Drop a column from a table in the database
+def drop_column(conn, schemaName, tableName, columnName):
+    cur = conn.cursor()
+
+    drop_column_query = """
+        alter table """ + schemaName + "." + tableName +  """
+        drop column """ + columnName + """;
+    """
+
+    cur.execute(drop_column_query)
+    cur.close()
