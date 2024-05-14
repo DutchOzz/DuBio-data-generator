a71ef1a333ebd1e37056caab963d4a38de1661a8
DutchOzz <m.schut@student.utwente.nl>
2024-05-14 10:28:00 +0200

    removed try catch from connect.py

--- connect.py
+++ connect.py
@@ -1,27 +1,21 @@
 import psycopg2
-# import sys
-
-# sys.path.append('C:\Users\matie\Documents\Matteo\UT\mod12\code\Autogenerater')
-
 import config
 
 conn = None
 
 def connect():
-    try:
-        global conn
-        conn = psycopg2.connect (
-            host = config.hostname, 
-            database = config.database, 
-            user = config.username, 
-            password = config.pwd, 
-            port = config.port_id
-            )    
-    except:
-        print("I am unable to connect to the database")
+    global conn
+    conn = psycopg2.connect (
+        host = config.hostname, 
+        database = config.database, 
+        user = config.username, 
+        password = config.pwd, 
+        port = config.port_id
+        )
 
 def close():
     conn.close()
 
 connect()
+print("Connected to database")
 close()
