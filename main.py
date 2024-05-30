0d6033c67a25ed09ec8fca2ed1d9299a6d92e037
DutchOzz <m.schut@student.utwente.nl>
2024-05-30 11:51:31 +0200

    random bs

--- main.py
+++ main.py
@@ -6,24 +6,16 @@ import tableFunctions as tf
 import queryFunctions as qf
 
 schemaName = "testschema"
+columnAmount = 10
 dictionarySize = 100
 rowCount = 2520
 amountOfPossibilities = 10
 conn = c.connect()
 bddSize = 1
-rowCountFrank = 10
-
-df.dropDictionary(conn, schemaName)
-df.createDictionary(conn, schemaName)
-df.addDictionaryEntries(conn, schemaName, dictionarySize // amountOfPossibilities, amountOfPossibilities)
 
 tf.drop_table(conn, schemaName, "with")
-tf.create_table(conn, schemaName, "with")
-tf.add_column(conn, schemaName, "with", "probability", "FLOAT")
-tdf.insertRowsWithCache(conn, schemaName, "with", rowCountFrank, dictionarySize // amountOfPossibilities, amountOfPossibilities, bddSize, '&', "Frank")
-tdf.insertRowsWithCache(conn, schemaName, "with", rowCount-rowCountFrank, dictionarySize // amountOfPossibilities, amountOfPossibilities, bddSize, '&')
+tf.create_table(conn, schemaName, "with", columnAmount)
+tdf.insertRows(conn, schemaName, "with", columnAmount, rowCount, dictionarySize, amountOfPossibilities, bddSize=bddSize)
 
-conn.commit()
-qf.smartUpdateDictionaryWithCache(conn, schemaName, "with")
 conn.commit()
 c.close(conn)
